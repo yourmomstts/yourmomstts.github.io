a2083a06302e57653192d12129888a6416f9e52a
momstts <yourmomstts@gmail.com>
2022-04-20 00:46:06 -0700

    Google Analytics added

---
+++ _site/docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "(OLD VIDEO) Everyone! 2nd Batch of TownStar Nodes by GalaGames is soon on sale!",
+        "excerpt":"Hello everybody that’s Your Mom's TTS on the line. The best information from forums, news and books compiled in one Your Mom’s TTS YouTube channel and read by the best electronic voice in YouTube. Attention!!! Sale of the SECOND batch of Nodes for the Townstar game from the Gala Games...","categories": ["crypto"],
+        "tags": [],
+        "url": "/crypto/2022/04/04/Everyone!-2nd-Batch-of-TownStar-Nodes-by-GalaGames-is-soon-on-sale.html",
+        "teaser": null
+      },{
+        "title": "Founder's Gala Node Investment for a Reasonable money All details",
+        "excerpt":"EASTER EGG (how many nodes left): EASTER EGG Hello everybody that’s Your Mom's TTS on the line. The best information from forums, news and books compiled in one Your Mom’s TTS YouTube channel and read by the best electronic voice in YouTube. ##################################################### !!! DISCLAIMER: With all the desire to...","categories": ["crypto"],
+        "tags": [],
+        "url": "/crypto/2022/04/04/Founders-Gala-Nod-Investment-for-a-Reasonable-money-All-details.html",
+        "teaser": null
+      },{
+        "title": "How many Gala Nodes left AT THIS MOMENT?",
+        "excerpt":"Hello everybody that’s Your Mom's TTS on the line. The best information from forums, news and books compiled in one Your Mom’s TTS YouTube channel and read by the best electronic voice in YouTube. ##################################################### !!! DISCLAIMER: With all the desire to provide you with the most relevant and valuable...","categories": ["crypto"],
+        "tags": [],
+        "url": "/crypto/2022/04/04/How-many-Gala-Nodes-left-AT-THIS-MOMENT.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Your Mom's TTS!",
+        "excerpt":"Hello everybody that’s Your Mom's TTS on the line. The best information from forums, news and books compiled in one Your Mom’s TTS YouTube channel and read by the best electronic voice in YouTube.   ","categories": ["youtube","channel"],
+        "tags": [],
+        "url": "/youtube/channel/2022/04/04/what-this-channel-about.html",
+        "teaser": null
+      }]
